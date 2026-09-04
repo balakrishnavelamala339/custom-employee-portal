e3c5539ae17f6623462c3ba4b2155c50c4586e11
Balakrishna <balakrishnavelamala339@gmail.com>
2026-09-04 10:22:33 +0530

    Switch to Postgres

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const path = require('path');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '../../database.sqlite'),
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
 });
 
